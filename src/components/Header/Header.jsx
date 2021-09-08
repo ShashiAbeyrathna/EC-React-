32e1c93496527f3f3b32f1b05a93be2e28bc3d69
ShashiAbeyrathna <36063032+ShashiAbeyrathna@users.noreply.github.com>
2021-09-08 15:31:50 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import useStyles from "./style";
+import HomeIcon from '@material-ui/icons/Home';
+
+export default function AppBarHeader() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar>
+        <div className={classes.toolbarIcon}>
+          <Avatar className={classes.logo} src="/logo.png"/>
+          <h2 spacing={1}>My React App&nbsp;</h2>
+          <Button color="default" className={classes.button} variant="outlined" href="/"><HomeIcon/>Home</Button>
+        </div>
+          <Typography variant="h6" className={classes.title}>
+            
+          </Typography>
+          <Button color="inherit" href="/login">Signin</Button>
+          <Button color="inherit" href="/register">Signup</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
